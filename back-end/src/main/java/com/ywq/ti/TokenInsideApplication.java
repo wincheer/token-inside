6c7a7bc0c29cfcc83c2d404c8b7a13a86844a60d
wincheer <wincheer@126.com>
2018-06-12 17:23:25 +0800

    修改springboot独立运行与服务器部署配置

--- back-end/src/main/java/com/ywq/ti/TokenInsideApplication.java
+++ back-end/src/main/java/com/ywq/ti/TokenInsideApplication.java
@@ -3,15 +3,22 @@ package com.ywq.ti;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @ServletComponentScan
 @MapperScan(basePackages = "com.ywq.ti.dao")
 @SpringBootApplication(scanBasePackages = "com.ywq.ti")
 @EnableScheduling
-public class TokenInsideApplication {
+public class TokenInsideApplication extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TokenInsideApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TokenInsideApplication.class, args);
 	}
